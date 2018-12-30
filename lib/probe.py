d11452b73e3532a8842e95c9309701afe4c485f6
Antti Jaakkola <antti.jaakkola@remod.fi>
2018-12-30 20:04:14 +0200

    start multiprobes

--- lib/probe.py
+++ lib/probe.py
@@ -99,8 +99,8 @@ def create_multi_probe(host, probe, options, start=False):
         if reload:
             logger.warn("Adding new multi probe %s" % probe)
         multi_probes_dict[probe] = multi_probe_cache[probe]()
-        logger.warn("Starting multi probe %s" % probe)
         if start:
+            logger.warn("Starting multi probe %s" % probe)
             multi_probes_dict[probe].start()
 
     multi_probes_dict[probe].add_host(host, name)
